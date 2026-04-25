90f237e986704b139555b2bc62115bfa5f1b405f
MJCopper <matt@thecoppers.net>
2026-04-26 08:51:15 +1000

    Version Change

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     name        = "casualkochess",
     fullname    = _("Casual KO Chess"),
-    description = _([[Casual chess game for KOReader. Author: MJCopper. Version: 0.1.5]]),
+    description = _([[Casual chess game for KOReader. Author: MJCopper. Version: 0.1.6]]),
 }
